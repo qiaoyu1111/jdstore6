39890b8d1a5c7c5f6bc6ea18deceee3547834cf5
qiaoyu <qiaoyu1111358@gmail.com>
2017-06-19 15:52:38 +0800

    implement cart step 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615044644) do
+ActiveRecord::Schema.define(version: 20170619073848) do
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer  "cart_id"
+    t.integer  "product_id"
+    t.integer  "quantity",   default: 1
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -19,7 +32,6 @@ ActiveRecord::Schema.define(version: 20170615044644) do
     t.integer  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
